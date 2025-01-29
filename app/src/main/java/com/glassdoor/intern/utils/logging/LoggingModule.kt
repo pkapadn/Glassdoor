03cebe068d979b869aa3390115765f92326b0a4f
Pranav Kapadne <pranav@Pranavs-MacBook-Pro.local>
2025-01-29 10:02:49 -0500

    dependecies issues fixed

---
+++ app/src/main/java/com/glassdoor/intern/utils/logging/LoggingModule.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025, Glassdoor Inc.
+ *
+ * Licensed under the Glassdoor Inc Hiring Assessment License.
+ * You may not use this file except in compliance with the License.
+ * You must obtain explicit permission from Glassdoor Inc before sharing or distributing this file.
+ * Mention Glassdoor Inc as the source if you use this code in any way.
+ */
+
+package com.glassdoor.intern.utils.logging
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoSet
+import timber.log.Timber
+
+/**
+ * TODO: [Annotate the DI module](https://developer.android.com/training/dependency-injection/hilt-android#hilt-modules) with correct definitions and [component](https://developer.android.com/training/dependency-injection/hilt-android#hilt-modules)
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+internal object LoggingModule {
+
+    @IntoSet
+    @Provides
+    fun provideTimberTree(): Timber.Tree =
+        Timber.DebugTree()
+}
